06584f004d46ef527133651a91515c716b8e448d
zouzihuan14 <1046055047@qq.com>
2020-05-31 22:18:12 +0800

    抽象语法树

--- node/http-browser-process/parse-dom.js
+++ node/http-browser-process/parse-dom.js
@@ -1,16 +1,46 @@
 let htmlStr = `<html>
   <head></head>
   <body>
+    <img />
+    <span></span>
     <div></div>
   </body>
 </html>`
-// 可以：KMP 正则 原理都是 FSM
-// 正则 split(): 完整的 html 
-// token: html tag
 let currentToken = null;
+
+let stack = [ { type: 'document', children: [] } ];
 parse(htmlStr);
+console.log(JSON.stringify(stack[0], null, 2));
 function emit(token) {
   console.log(token);
+  let top = stack[stack.length - 1];
+  if (token.type === 'startTag') {
+    // push pop 处理配对
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: [],
+      tagName: token.tagName
+    }
+    stack.push(element);
+    // 作为栈顶的元素子节点，为了生成树
+    // if (!top.children) top.children = [];
+    top.children.push(element);
+  } else if (token.type === 'endTag') {
+    if (token.tagName !== top.tagName) {
+      throw new Error('tagname match error')
+    } else {
+      stack.pop();
+    }
+  } else if (token.type === 'selfCloseToken') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: [],
+      tagName: token.tagName
+    }
+    top.children.push(element);
+  }
   currentToken = null;
 }
 function parse(htmlString) {
@@ -45,10 +75,18 @@ function tagName(c) {
   if (c.match(/[a-zA-Z]/)) {
     currentToken.tagName += c;
     return tagName
+  } else if (c.match(/[\t\n\f ]/)) {
+    return beforeAttaibuteName
   } else if (c === '>') {
     // tag 拼接结束
     emit(currentToken);
     return start
+  }
+}
+function beforeAttaibuteName(c) {
+  if (c === '/') {
+    currentToken.type = 'selfCloseToken';
+    return tagName;
   }
 }
 function endTagOpen(c) {
