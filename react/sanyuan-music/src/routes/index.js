729fb01a3bb9c1067affe1cfdeb76e9dcf4a03f5
zouzihuan14 <1046055047@qq.com>
2020-08-06 06:42:15 +0800

    8/6

--- react/sanyuan-music/src/routes/index.js
+++ react/sanyuan-music/src/routes/index.js
@@ -1,58 +1,35 @@
-// 路由的本质是什么？
-// 路由级别的页面
-// 二级路由 
-// /path Component 放在相应的地方<Route />
-// 嵌套
-import React, { lazy, Suspense } from 'react';
-import { Redirect } from 'react-router-dom';
-import BlankLayout from '../layouts/BlankLayout';
-import HomeLayout from '../layouts/HomeLayout';
-// import Recommend from '../application/Recommend/';
-const RecommendComponent = lazy(() => import("../application/Recommend/"))
-const SingersComponent = lazy(() => import("../application/Singers/"))
-const SingerComponent = lazy(() => import("./../application/Singer/"));
+//routes/index.js
+import React from 'react';
+import { Redirect } from "react-router-dom";
+import Home from '../application/Home';
+import Recommend from '../application/Recommend';
+import Singers from '../application/Singers';
+import Rank from '../application/Rank';
 
-const SuspenseComponent = Component => props => {
-  return (
-    <Suspense fallback={null}>
-      <Component {...props}>
-      </Component>
-    </Suspense>
-  )
-}
-
-export default [{
-  component: BlankLayout,
-  routes: [
-    {
-      path: "/",
-      component: HomeLayout,
-      routes: [
-        {
-          path: '/',
-          exact: true,
-          render: () => <Redirect to={"/recommend"} />
-        },
-        {
-          path: '/recommend',
-          component: SuspenseComponent(RecommendComponent)
-        },
-        {
-          path: '/singers',
-          component: SuspenseComponent(SingersComponent),
-          key: "singers",
-          routes: [
-            {
-              path: "/singers/:id",
-              component: SuspenseComponent(SingerComponent)
-            }
-          ]
-        },
-        // {
-        //   path: '/rank',
-        //   component: RankComponent
-        // },
-      ]
-    }
-  ]
-}]
+export default [
+  {
+    path: "/",
+    component: Home,
+    routes: [
+      {
+        path: "/",
+        exact: true,
+        render: () => (
+          <Redirect to={"/recommend"}/>
+        )
+      },
+      {
+        path: "/recommend",
+        component: Recommend
+      },
+      {
+        path: "/singers",
+        component: Singers
+      },
+      {
+        path: "/rank",
+        component: Rank
+      }
+    ]
+  }
+]
